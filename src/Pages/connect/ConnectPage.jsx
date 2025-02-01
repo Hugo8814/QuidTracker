b1bae10724c6f733ee13ce1e14b41685a1a80cfb
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-02-01 19:35:13 +0000

    delete 3 files and update 2 files

--- src/Pages/connect/ConnectPage.jsx
+++ src/Pages/connect/ConnectPage.jsx
@@ -3,11 +3,13 @@
 // dotenv.config();
 
 function ConnectPage() {
-  const CLIENT_ID = "sandbox-quidtracker-48cd14";
-  const REDIRECT_URI = "http://localhost:5173/connect"; // Update to match your frontend URL
-  const AUTH_URL = `https://auth.truelayer.com/?response_type=code&client_id=${CLIENT_ID}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=${encodeURIComponent(
-    REDIRECT_URI
-  )}&providers=uk-ob-all%20uk-oauth-all`;
+  // const CLIENT_ID = "sandbox-quidtracker-48cd14";
+  // const REDIRECT_URI = "http://localhost:5173/connect"; // Update to match your frontend URL
+  const AUTH_URL = `https://auth.truelayer-sandbox.com/?response_type=code&client_id=sandbox-quidtracker-48cd14&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=http://localhost:5173/connect&providers=uk-cs-mock%20uk-ob-all%20uk-oauth-all`;
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const authorizationCode = urlParams.get("code");
+  console.log(authorizationCode);
 
   return (
     <div className="shadow-xl flex justify-center items-center p-5 h-screen bg-[#919190] max-1100:flex-col max-1100:p-0">
@@ -18,7 +20,7 @@ function ConnectPage() {
         Connect Bank
       </a>
       <a
-        href="/Overview"
+        href="/app"
         className="bg-blue-500 text-2xl hover:bg-gray-700 text-white font-bold py-6 px-4 rounded-xl focus:outline-none focus:shadow-outline max-380:text-xl"
       >
         skip
--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,35 +1,23 @@
-
 import NavBtn from "./NavBtn";
-
 
 function SideBar() {
   return (
     <div className="rounded-tr-3xl h-full flex flex-col p-12 gap-4 bg-gray-900 text-white max-1300:px-4 max-1100:flex-row  max-1100:p-[1rem] max-1100:h-auto max-500:text-transparent max-1100:rounded-tr-none max-1100:rounded-b-3xl max-1100:sticky max-1100:top-0 z-50  ">
       {/* Logo Section */}
       <div className="mt-4 mb-28  max-1100:hidden">
-        <img  alt="logo" />
+        <img alt="logo" />
       </div>
 
       {/* Navigation Links */}
-      <NavBtn to="Overview">
-        Overview
-      </NavBtn>
+      <NavBtn to="Dashboard">Dashboard</NavBtn>
 
-      <NavBtn to="Transactions">
-        Transactions
-      </NavBtn>
+      <NavBtn to="Transactions">Transactions</NavBtn>
 
-      <NavBtn to="Budgets">
-        Budgets
-      </NavBtn>
+      <NavBtn to="Budgets">Budgets</NavBtn>
 
-      <NavBtn  to="Pots">
-        Pots
-      </NavBtn>
+      <NavBtn to="Pots">Pots</NavBtn>
 
-      <NavBtn  to="RecurringBills">
-        Recurring Bills
-      </NavBtn>
+      <NavBtn to="RecurringBills">Recurring Bills</NavBtn>
     </div>
   );
 }
